a1c00ae3c0c1e0492cc985a7637bd46ceb034c33
Robert Vinluan <rvinluan@sidefx.com>
2024-10-29 09:41:03 -0400

    Fix header view styling such that centered text is actually centered.
    308845cd720969a5f3456caa8cf72023f120aecf broke this behavior
    when it took into account the space of the sorter arrow in the
    text size calculation.  The commit was meant to fix overlapping
    issues for right-aligned and bottom-aligned text,
    according to the commit log message, so we limit the scope of the
    change so that it only applies when the text alignment is set to
    either AlignRight or AlignBottom, depending on the header orientation.

--- src/widgets/styles/qstylesheetstyle.cpp
+++ src/widgets/styles/qstylesheetstyle.cpp
@@ -4106,8 +4106,12 @@ void QStyleSheetStyle::drawControl(ControlElement ce, const QStyleOption *opt, Q
           else
               v1Copy = *header;
             QRenderRule subRule = renderRule(w, opt, PseudoElement_HeaderViewSection);
-            if (hasStyleRule(w, PseudoElement_HeaderViewUpArrow)
-             || hasStyleRule(w, PseudoElement_HeaderViewDownArrow)) {
+            if ((hasStyleRule(w, PseudoElement_HeaderViewUpArrow)
+             || hasStyleRule(w, PseudoElement_HeaderViewDownArrow))
+	     && (hdr.orientation == Qt::Horizontal 
+		    && hdr.textAlignment & Qt::AlignRight
+		|| hdr.orientation == Qt::Vertical
+		    && hdr.textAlignment & Qt::AlignBottom)) {
                 const QRect arrowRect = subElementRect(SE_HeaderArrow, opt, w);
                 if (hdr.orientation == Qt::Horizontal)
                     hdr.rect.setWidth(hdr.rect.width() - arrowRect.width());
